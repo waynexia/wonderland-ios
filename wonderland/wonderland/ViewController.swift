b83dd7e3837aad10de3480018e1c5eaf5f11a5d4
Ruihang Xia <waynestxia@gmail.com>
2019-11-05 14:45:06 +0800

    add storyboard placeholder

---
+++ wonderland/wonderland/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wonderland
+//
+//  Created by imac on 2019/11/5.
+//  Copyright © 2019 SCUT_wonderland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
