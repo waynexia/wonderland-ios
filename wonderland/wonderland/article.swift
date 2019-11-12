3241478282ec38999f3ec0f6343c98aa8f3b425d
Ruihang Xia <waynestxia@gmail.com>
2019-11-12 16:25:41 +0800

    add article meta and body model

--- wonderland/wonderland/article.swift
+++ wonderland/wonderland/article.swift
@@ -2,25 +2,24 @@
 //  article.swift
 //  wonderland
 //
-//  Created by Apple on 2019/11/12.
+//  Created by Wenhao Fang on 2019/11/12.
 //  Copyright © 2019 SCUT_wonderland. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class article: NSObject, NSCoding {
-    var title: String?
-    var abstract: String?
-    var mainBody: String?
+class ArticleMeta: NSObject, NSCoding {
+    var title: String
+    var abstract: String
     var firstImage: UIImage?
-    var tags: Array<String>?
-    var categories: Array<String>?
-    var createdTime: NSDate?
+    var tags: Array<String>
+    var createdTime: NSDate
+    var contentIndex: UInt32
     static let DocumentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentDirectory.appendingPathComponent("acticles")
     
-    init(title: String?, abstract: String?, mainBody: String?, firstImage: UIImage?, tags: Array<String>?, categories: Array<String>?, createdTime: NSDate?) {
+    init(title: String, abstract: String, firstImage: UIImage?, tags: Array<String>, categories: Array<String>, createdTime: NSDate, contentIndex: UInt32) {
         self.title = title
         self.abstract = abstract
         self.mainBody = mainBody
@@ -29,8 +28,8 @@ class article: NSObject, NSCoding {
             self.firstImage = UIImage(named: "./img/default.jpg")
         }
         self.tags = tags
-        self.categories = categories
         self.createdTime = createdTime
+        self.contentIndex = contentIndex
     }
     
     func encode(with aCoder: NSCoder) {
@@ -39,17 +38,37 @@ class article: NSObject, NSCoding {
         aCoder.encode(mainBody, forKey: "mainBodyKey")
         aCoder.encode(firstImage, forKey: "firstImageKey")
         aCoder.encode(tags, forKey: "tagsKey")
-        aCoder.encode(categories, forKey: "categoriesKey")
         aCoder.encode(createdTime, forKey: "createdTimeKey")
+        aCoder.encode(contentIndex, forKey: "contentIndex")
     }
     
     required init?(coder aDecoder: NSCoder) {
-        title = aDecoder.decodeObject(forKey: "titleKey") as? String
-        abstract = aDecoder.decodeObject(forKey: "abstractKey") as? String
-        mainBody = aDecoder.decodeObject(forKey: "mainBodyKey") as? String
+        title = aDecoder.decodeObject(forKey: "titleKey")!
+        abstract = aDecoder.decodeObject(forKey: "abstractKey")!
+        mainBody = aDecoder.decodeObject(forKey: "mainBodyKey")!
         firstImage = aDecoder.decodeObject(forKey: "firstImageKey") as? UIImage
-        tags = (aDecoder.decodeObject(forKey: "tagsKey") as? Array<String>?) ?? []
-        categories = (aDecoder.decodeObject(forKey: "categoriesKey") as? Array<String>?) ?? []
-        createdTime = (aDecoder.decodeObject(forKey: "createdTimeKey") as? NSDate?)!
+        tags = aDecoder.decodeObject(forKey: "tagsKey")!
+        createdTime = (aDecoder.decodeObject(forKey: "createdTimeKey")!)!
+        contentIndex = (aDecoder.decodeObject(forKey: "contentIndex")!)
+    }
+}
+
+class Article: NSObject, NSCoding{
+    var content: String
+    
+    init(content: String){
+        self.content = content
+    }
+    
+    init(){
+        self.content = ""
+    }
+    
+    func encode(with aCoder: NSCoder){
+        aCoder.encode(content, forKey:"content")
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        content = aDecoder.decodeObject(forKey:"content")
     }
 }
