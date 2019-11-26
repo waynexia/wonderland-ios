1710b17e4021c164f1f82444bb3ab65b6e383030
Tomorrow-XiaoSong <13623090165@163.com>
2019-11-26 15:43:49 +0800

    mod: update detail view

--- wonderland/wonderland/DetailVC.swift
+++ wonderland/wonderland/DetailVC.swift
@@ -11,21 +11,44 @@ import MarkdownKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var detailTitle: UITextView!
+    @IBOutlet weak var detailCreateDate: UITextView!
+    @IBOutlet weak var detailTags: UITextView!
     @IBOutlet weak var detailText: UITextView!
-    var articleIndex: Int?
+    
+    // 跳转到detail界面，赋值articleIndex
+    var articleMeta: ArticleMeta?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let markdownParser = MarkdownParser(font: UIFont.systemFont(ofSize: 20))
-        let markdown = self.loadDetail()
-        self.detailText.attributedText = markdownParser.parse(markdown ?? "")
+        if(self.articleMeta != nil){
+            // title
+            self.detailTitle.text = self.articleMeta!.title
+            // create time
+            self.detailCreateDate.text = self.articleMeta!.createdTime.description
+            // tags
+            var tagsText = ""
+            for index in 0..<self.articleMeta!.tags.count{
+                tagsText += (self.articleMeta!.tags[index] + " ")
+            }
+            self.detailTags.text = tagsText
+            // main content
+            let markdownParser = MarkdownParser(font: UIFont.systemFont(ofSize: 20))
+            let content = self.loadDetail(contentIndex: self.articleMeta!.contentIndex)
+            self.detailText.attributedText = markdownParser.parse(content ?? "")
+        }
+        else{
+            print("articleMeta is nil")
+        }
     }
     
-    func loadDetail() -> String?{
-        if(self.articleIndex != nil){
+    // 加载detailText的文本内容（index为nil，或文章不存在，返回nil）
+    func loadDetail(contentIndex: UInt32) -> String?{
+        if(contentIndex != nil){
             var articlesList = NSKeyedUnarchiver.unarchiveObject(withFile: ArticlesURL.path) as? [String]
             if(articlesList != nil){
-                return articlesList?[self.articleIndex!]
+                return articlesList?[Int(contentIndex)]
             }
             else{
                 print("userLocalArticlesFile is nil")
