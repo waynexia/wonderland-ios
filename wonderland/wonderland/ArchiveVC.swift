f9eb03785e90a78901c622ecc898c889e3ab1a12
Ruihang Xia <waynestxia@gmail.com>
2019-11-26 15:15:40 +0800

    add ArchiveVC init func

--- wonderland/wonderland/ArchiveVC.swift
+++ wonderland/wonderland/ArchiveVC.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class ArchiveVC: UIViewController {
-    // tags field
-    var tags : [Tag] = [Tag]()
+    // catalog fields
+    var months : [MonthCatalog]?
+    var tags: [Tag]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,39 @@ class ArchiveVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // call this function when jumping to this view
+    func setup(metas: [ArticleMeta]){
+        self.months = calc_month(metas: metas)
+        self.tags = calc_tag(metas: metas)
+    }
+    
+    func calc_month(metas: [ArticleMeta]) -> [MonthCatalog]{
+        var tags = [UInt32:MonthCatalog]()
+        for meta in metas {
+            // meta.createdTime
+            let (month,year) = from_NSDate(date: meta.createdTime)
+            let time_hash = month * 10000 + year
+            
+            if tags[time_hash] != nil{
+                tags[time_hash]?.count += 1
+            } else {
+                tags[time_hash] = MonthCatalog(month: UInt8(month), year: year, count: 1)
+            }
+            
+        }
+        return Array(tags.values)
+    }
+    
+    func calc_tag(metas: [ArticleMeta]) -> [Tag]{
+        var tags = [String : Tag]()
+        for meta in metas{
+            for tag in meta.tags{
+                tags[tag]?.tag_name = tag
+                tags[tag]?.tag_count += 1
+            }
+        }
+        return Array(tags.values)
+    }
 
     /*
     // MARK: - Navigation
@@ -29,4 +63,19 @@ class ArchiveVC: UIViewController {
     }
     */
 
+}
+
+// Utils
+
+// extract month and year from a NSDate
+func from_NSDate(date: NSDate) -> (UInt32,UInt32){
+    let formatter : DateFormatter = DateFormatter()
+    let str : String = formatter.string(from: date as Date)
+    //        let ret_month = UInt8(str[0...4])
+    let ret_month = UInt32(str.prefix(4))!
+    //        let ret_year = UInt32(str[5...7])
+    let ret_year = UInt32(str[str.index(str.startIndex,offsetBy: 5)..<str.index(str.startIndex, offsetBy: 7)])!
+    
+    return (ret_month,ret_year)
+    
 }
---
+++ wonderland/wonderland/ContentEntry.swift
@@ -0,0 +1,14 @@
+//
+//  ContentEntry.swift
+//  wonderland
+//
+//  Created by Waynest on 2019/11/12.
+//  Copyright © 2019 SCUT_wonderland. All rights reserved.
+//
+
+import Foundation
+
+struct ContentEntry{
+    var index : UInt32
+    var title : String
+}
